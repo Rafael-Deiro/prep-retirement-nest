db29b803836199fc0762287af42c8eca87409c49
Rafael Deiró <rafaeldeirolopes@gmail.com>
2024-12-14 01:32:18 -0300

    feat: Enable versioning

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
